d4aebdf302b8af8aa1427d5e3ceb55cffc8e10d2
Charles Burnatowski <charles.burna@hotmail.fr>
2023-06-24 02:55:27 +0200

    Nettoyage commentaires

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,8 @@
-const arrowLeftElement = document.querySelector('.arrow_left');
-const arrowRightElement = document.querySelector('.arrow_right');
 const bannerImageElement = document.querySelector('.banner-img');
 const bannerParagraphElement = document.querySelector('#banner p');
-
-// Test pour les dots
-
-const dotElements = document.getElementsByClassName('dot');  // liste de boutons
-const dotSelected = document.querySelector('.dot_selected') // inutile ? 
-
+const arrowLeftElement = document.querySelector('.arrow_left');
+const arrowRightElement = document.querySelector('.arrow_right');
+const dotElements = document.getElementsByClassName('dot');
 
 let index = 0;
 
@@ -38,7 +33,6 @@ arrowRightElement.addEventListener('click', () => {
   } else {
     index++
   }
-  console.log(index);
   bannerImageElement.src = `./assets/images/slideshow/${slides[index].image}`;
   bannerParagraphElement.innerHTML = slides[index].tagLine;
   document.getElementById('dot'+[index]).classList.add('dot_selected');
@@ -51,7 +45,6 @@ arrowLeftElement.addEventListener('click', () => {
   } else {
     index--
   }
-  console.log(index);
   bannerImageElement.src = `./assets/images/slideshow/${slides[index].image}`;
   bannerParagraphElement.innerHTML = slides[index].tagLine;
   document.getElementById('dot'+[index]).classList.add('dot_selected');
@@ -59,14 +52,12 @@ arrowLeftElement.addEventListener('click', () => {
 
 const dotPressed = e => {
   document.getElementById('dot'+[index]).classList.remove('dot_selected');
-  index = e.target.id.substring(3);  // substring ==> index = 'dot'+ [index], - trois lettres du string, soit le "dot", donc = chiffre
-  console.log(index);
-  console.log(e.target.id);  // Get ID of Clicked Element
+  index = e.target.id.substring(3); 
   bannerImageElement.src = `./assets/images/slideshow/${slides[index].image}`;
   bannerParagraphElement.innerHTML = slides[index].tagLine;
   document.getElementById('dot'+[index]).classList.add('dot_selected');
 }
 
 for (let dotElement of dotElements) {
-  dotElement.addEventListener("click", dotPressed); // Rend les boutons clickables, après avoir créé la fonction e
+  dotElement.addEventListener("click", dotPressed);
 }
